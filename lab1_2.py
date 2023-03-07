e104e9e0f4f67d114d1e8bb3f81b8fa7f3a7e2c7
rauf112 <rauf112@gmail.com>
2023-03-07 11:40:49 +0300

    commit

---
+++ lab1_2.py
@@ -0,0 +1,12 @@
+var1 = input("Enter Number 1" '\n')
+var2 = input("Enter Number 2" '\n')
+
+sum = float(var1) + float(var2)
+diff = float(var1) - float(var2)
+prod = float(var1) * float(var2)
+
+print("var1=", float(var1))
+print("var2=", float(var2))
+print("summation =", float(sum))
+print("difference =", float(diff))
+print("multiplication =", float(prod))
